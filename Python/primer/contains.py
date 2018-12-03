94d716e384e21d6958ad768a6e80e40c28751379
sonaspy <sonaspy@zju.edu.cn>
2018-12-04 00:06:35 +0800

    q

---
+++ Python/primer/contains.py
@@ -0,0 +1,8 @@
+# author - newguo@sonaspy.cn 
+# coding - utf_8 
+
+def contains(data, target):
+  for item in target:
+    if item == target:               # found a match
+      return True
+  return False
