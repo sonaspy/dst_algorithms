7890a3e63fe4fe312ae220ad496e367633b3d15c
NewGuonx <gnx0406@gmail.com>
2018-12-04 20:41:54 +0800

    cc

--- C++V2/stack/stack_vector.h
+++ C++V2/stack/stack_vector.h
@@ -4,5 +4,5 @@ template <typename T> class Stack: public Vector<T> {
 public:  
    void push ( T const& e ) { insert ( size(), e ); }  
    T pop() { return remove ( size() - 1 ); }  
-   T& top() { return ( 
+   T& top() { return  last()->data; }
 };
