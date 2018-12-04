5d582f514b8cd8e18922fba4c7c065e3e8b785a1
sonaspy <sonaspy@zju.edu.cn>
2018-12-04 20:41:54 +0800

    cc

---
+++ C++V2/queue/queue.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../list/List.h"  
+template <typename T> class Queue: public List<T> {  
+public:  
+   void enqueue ( T const& e ) { insertAsLast ( e ); }  
+   T dequeue() { return remove ( first() ); }  
+   T& front() { return first()->data; }  
+};
+
+#include "queue_implementation.h"
