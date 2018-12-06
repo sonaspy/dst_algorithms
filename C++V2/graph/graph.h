d000c8d8b614c535a5bc659bc64e166f2b6d82b6
NewGuonx <gnx0406@gmail.com>
2018-12-06 15:28:12 +0800

    g

--- C++V2/graph/graph.h
+++ C++V2/graph/graph.h
@@ -45,9 +45,172 @@ public:
  
    void bfs ( int );  
    void dfs ( int );  
-   void bcc ( int );  
+   void bcc ( int ); 
+   void PFS(int s, PU prioUpdater) ;
    Stack<Tv>
    void prim ( int );  
    void dijkstra ( int );  
-   template <typename PU> void pfs ( int, PU );  
+   template <typename PU> void pfs ( int, PU );
 };
+
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::bfs(int s)
+{
+   reset();
+   int clock = 0;
+   int v = s;
+   do
+      if (UNDISCOVERED == status(v))
+         BFS(v, clock);
+   while ( (v = (++v % n)) != s);
+}
+
+
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::BFS(int v, int &clock)
+{
+   Queue<int> Q;
+   status(v) = DISCOVERED;
+   Q.enqueue(v);
+   while (!Q.empty())
+   {
+      int v = Q.dequeue();
+      dTime(v) = ++clock;
+      for (int u = firstNbr(v); - 1 < u; u = nextNbr(v, u))
+         if (UNDISCOVERED == status(u))
+         {
+            status(u) = DISCOVERED;
+            Q.enqueue(u);
+            type(v, u) = TREE;
+            parent(u) = v;
+         }
+         else
+            type(v, u) = CROSS;
+      // finish
+      status(v) = VISITED;
+   }
+}
+
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::dijkstra(int s)
+{
+   reset();
+   priority(s) = 0;
+   for (int i = 0; i < n; i++)
+   {
+      status(s) = VISITED;
+      if (-1 != parent(s))
+         type(parent(s), s) = TREE;
+      for (int j = firstNbr(s); - 1 < j; j = nextNbr(s, j))
+         if ((status(j) == UNDISCOVERED) && (priority(j) > priority(s) + weight(s, j)))
+         {
+            priority(j) = priority(s) + weight(s, j);
+            parent(j) = s;
+         }
+      for (int shortest = INT_MAX, j = 0; j < n; j++)
+         if ((status(j) == UNDISCOVERED) && (shortest > priority(j)))
+         {
+            shortest = priority(j);
+            s = j;
+         }
+   }
+}
+
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::prim(int s)
+{
+   reset();
+   priority(s) = 0;
+   for (int i = 0; i < n; i++)
+   {
+      status(s) = VISITED;
+      if (-1 != parent(s))
+         type(parent(s), s) = TREE;
+      for (int j = firstNbr(s); - 1 < j; j = nextNbr(s, j))
+         if ((status(j) == UNDISCOVERED) && (priority(j) > weight(s, j)))
+         {
+            priority(j) = weight(s, j);
+            parent(j) = s;
+         }
+      for (int shortest = INT_MAX, j = 0; j < n; j++)
+         if ((status(j) == UNDISCOVERED) && (shortest > priority(j)))
+         {
+            shortest = priority(j);
+            s = j;
+         }
+   }
+}
+
+template <typename Tv, typename Te>
+template <typename PU>
+void Graph<Tv, Te>::pfs(int s, PU prioUpdater)
+{
+   reset();
+   int v = s;
+   do
+      if (UNDISCOVERED == status(v))
+         PFS(v, prioUpdater);
+   while (s != (v = (++v % n)));
+}
+
+template <typename Tv, typename Te>
+template <typename PU>
+void Graph<Tv, Te>::PFS(int s, PU prioUpdater)
+{
+   priority(s) = 0;
+   status(s) = VISITED;
+   parent(s) = -1;
+   while (1)
+   {
+      for (int w = firstNbr(s); - 1 < w; w = nextNbr(s, w))
+         prioUpdater(this, s, w);
+      for (int shortest = INT_MAX, w = 0; w < n; w++)
+         if (UNDISCOVERED == status(w))
+            if (shortest > priority(w))
+            {
+               shortest = priority(w);
+               s = w;
+            }
+      if (VISITED == status(s))
+         break;
+      status(s) = VISITED;
+      type(parent(s), s) = TREE;
+   }
+}
+
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::dfs(int s)
+{
+   reset();
+   int clock = 0;
+   int v = s;
+   do
+      if (UNDISCOVERED == status(v))
+         DFS(v, clock);
+   while (s != (v = (++v % n)));
+}
+
+
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::DFS(int v, int &clock)
+{
+   dTime(v) = ++clock;
+   status(v) = DISCOVERED;
+   for (int u = firstNbr(v); - 1 < u; u = nextNbr(v, u))
+      switch (status(u))
+      {
+      case UNDISCOVERED:
+         type(v, u) = TREE;
+         parent(u) = v;
+         DFS(u, clock);
+         break;
+      case DISCOVERED:
+         type(v, u) = BACKWARD;
+         break;
+      default:
+         type(v, u) = (dTime(v) < dTime(u)) ? FORWARD : CROSS;
+         break;
+      }
+   status(v) = VISITED;
+   fTime(v) = ++clock;
+}
