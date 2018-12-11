646bf6510fd3d5816c392bc4c3a153b373f072e6
NewGuonx <gnx0406@gmail.com>
2018-12-11 19:51:29 +0800

    integrate

--- Python/primer/contains.py
+++
@@ -1,8 +0,0 @@
-# author - newguo@sonaspy.cn 
-# coding - utf_8 
-
-def contains(data, target):
-  for item in target:
-    if item == target:               # found a match
-      return True
-  return False
