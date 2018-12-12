6ee2a9804fad5a95dce9e9f292d20f9ba07f4f81
NewGuonx <gnx0406@gmail.com>
2018-12-12 23:10:38 +0800

    sdq

---
+++ Python/S_Q_DQ/reverse_file.py
@@ -0,0 +1,18 @@
+# author - newguo@sonaspy.cn
+# coding - utf_8
+
+from array_stack import *
+
+
+def reverse_file(filename):
+    """Overwrite given file with its contents line-by-line reversed."""
+    S = ArrayStack()
+    original = open(filename)
+    for line in original:
+        S.push(line.rstrip('\n'))     # we will re-insert newlines when writing
+    original.close()
+    # now we overwrite with contents in LIFO order
+    output = open(filename, 'w')    # reopening file overwrites original
+    while not S.is_empty():
+        output.write(S.pop() + '\n')  # re-insert newline characters
+    output.close()
---
+++ Python/myExceptions.py
@@ -0,0 +1,6 @@
+# author - newguo@sonaspy.cn 
+# coding - utf_8 
+
+class Empty(Exception):
+    '''error attempting to access an element from an empty container '''
+    pass
