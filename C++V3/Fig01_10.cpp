60ed3e6191eaaa3d19fb893380ced9030f326166
sonaspy <sonaspy@zju.edu.cn>
2019-03-10 04:50:48 +0800

    ⌨️🖱️ improved code structure -

---
+++ C++V3/Fig01_10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main( )
+{
+    vector<int> squares( 100 );
+    for( int i = 0; i < squares.size( ); ++i )
+        squares[ i ] = i * i;
+    for( int i = 0; i < squares.size( ); ++i )
+        cout << i << " " << squares[ i ] << endl;
+    return 0;
+}
