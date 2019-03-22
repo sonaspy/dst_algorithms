0acc92cf07d2b8a706fe1dfed4b0126431bc3e3b
NewGuonx <gnx0406@gmail.com>
2019-03-23 00:09:35 +0800

    template

--- demo.cpp
+++ demo.cpp
@@ -1,5 +1,4 @@
-// author - newguo@sonaspy.cn
-// coding - utf_8
+
 
 #include <iostream>
 #include <vector>
@@ -38,9 +37,6 @@ class Fibonacci<1>
     };
 };
 
-
-
-
 template <int p, int i>
 struct PrimeMagicCube
 {
@@ -60,16 +56,16 @@ struct PrimeMagicCube<p, 1>
 template <int i>
 struct Number
 {
-    Number<i - 1> a; //  递归定义数值对象
+    Number<i - 1> a;
     enum
     {
         answer = PrimeMagicCube<i, i - 1>::answer
     };
     void IsPrime()
-    { //  先降序输出全部素数，后升序输出全部数值素性序列
+    {
         if (answer)
             v.push_back(i);
-        a.IsPrime(); //  递归调用，计算下一数值的素性
+        a.IsPrime();
     }
 };
 
@@ -86,14 +82,98 @@ struct Number<2>
     }
 };
 
+
+
+
+
+
+
+class Empty
+{
+};
+
+
+template <typename EventAction>
+class EventResponsor
+{
+  public:
+    EventResponsor() : actor(NULL), action(NULL) {}
+    EventResponsor(Empty *actor, EventAction *action) : actor(actor), action(action) {}
+    friend bool operator==(const EventResponsor &lhs, const EventResponsor &rhs)
+    {
+        return lhs.actor == rhs.actor && *lhs.action == *rhs.action;
+    }
+
+  public: 
+    Empty *actor;
+    EventAction *action;
+}; 
+
+
+template <typename EventAction>
+class Event
+{
+  public:
+    typedef vector<EventResponsor<EventAction> > EventResponsors;
+    typedef typename vector<EventResponsor<EventAction> >::iterator EventIterator;
+
+  public:
+    virtual ~Event()
+    {
+        for (EventIterator it = this->_ers.begin(); it != this->_ers.end(); ++it)
+        {
+            delete it->action, it->action = NULL;
+        }
+    }
+
+    EventResponsors &GetResponsors() { return this->_ers; }
+
+    template <typename Responsor, typename Action>
+    void Bind(Responsor *actor, Action action)
+    {
+        Action *act = new Action(action);
+        EventResponsor<EventAction> er((Empty *)actor, (EventAction *)act);
+        bool unbound = true;
+        for (EventIterator it = this->_ers.begin(); it != this->_ers.end(); ++it)
+        {
+            if (*it == er) 
+            {
+                unbound = false;
+                break;
+            }
+        }
+        if (unbound)
+            this->_ers.push_back(er);
+        else
+            delete er.action, er.action = NULL;
+    } 
+    template <typename Responsor, typename Action>
+    void Unbind(Responsor *actor, Action action)
+    {
+        Action *act = new Action(action);
+        EventResponsor<EventAction> er((Empty *)actor, (EventAction *)act);
+        for (EventIterator it = this->_ers.begin(); it != this->_ers.end(); ++it)
+        {
+            if (*it == er) 
+            {
+                delete it->action, this->_ers.erase(it);
+                break;
+            }
+        }
+        delete er.action, er.action = NULL;
+    }
+
+  private:
+    EventResponsors _ers;
+}; 
+
+
+
+
+
 int main(int argc, char const *argv[])
 {
     /* code */
-    //test();
 
-    Number<> a;
-    a.IsPrime();
-    for(auto i : v) cout << i << " ";
     return 0;
 }
-
