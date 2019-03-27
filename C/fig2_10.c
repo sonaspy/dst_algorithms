9b7896357b98b0ed9b314a0bc7800865ab2a5dd1
NewGuonx <gnx0406@gmail.com>
2019-03-27 18:33:09 +0800

    tsinghua djh

--- C/fig2_10.c
+++ C/fig2_10.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /* START: fig2_10.txt */
-unsigned int
-Gcd(unsigned int M, unsigned int N)
+inline unsigned int Gcd(unsigned int M, unsigned int N)
 {
     unsigned int Rem;
     /* 1*/ while (N > 0)
-        {
-            /* 2*/ Rem = M % N;
-            /* 3*/ M = N;
-            /* 4*/ N = Rem;
-        }
+    {
+        /* 2*/ Rem = M % N;
+        /* 3*/ M = N;
+        /* 4*/ N = Rem;
+    }
     /* 5*/ return M;
 }
 /* END */
