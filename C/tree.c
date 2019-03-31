f2eadef7acde22ef4b44228ad5647650cbb9cc50
NewGuonx <gnx0406@gmail.com>
2019-03-31 21:50:03 +0800

    update

--- C/tree.c
+++ C/tree.c
@@ -89,37 +89,29 @@ Insert(DataType X, SearchTree T)
 }
 /* END */
 
-/* START: fig4_25.txt */
-SearchTree
-Delete(DataType X, SearchTree T)
+
+SearchTree Delete(DataType X, SearchTree T)
 {
-    Position TmpCell;
-    if (T == NULL)
-        Error("Element not found");
+    Position tmp_node;
+    if (!T) Error("Element not found");
     else if (X < T->Element) /* Go left */
         T->Left = Delete(X, T->Left);
     else if (X > T->Element) /* Go right */
         T->Right = Delete(X, T->Right);
-    else                         /* Found element to be deleted */
-        if (T->Left && T->Right) /* Two children */
+    else if (T->Left && T->Right) /* Two children */
     {
-        /* Replace with smallest in right subtree */
-        TmpCell = FindMin(T->Right);
-        T->Element = TmpCell->Element;
+        tmp_node = FindMin(T->Right);
+        T->Element = tmp_node->Element;
         T->Right = Delete(T->Element, T->Right);
     }
     else /* One or zero children */
     {
-        TmpCell = T;
-        if (T->Left == NULL) /* Also handles 0 children */
-            T = T->Right;
-        else if (T->Right == NULL)
-            T = T->Left;
-        free(TmpCell);
+        tmp_node = T;
+        T =( T->Left == NULL? T->Right : T->Left);
+        free(tmp_node);
     }
     return T;
 }
-/* END */
 
 DataType
 Retrieve(Position P)
--- demo.cpp
+++ demo.cpp
@@ -1,21 +1,51 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
+#include <bits/stdc++.h>
 
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<string>
-#include<unordered_map>
-#include<stack>
-#include<queue>
-#include<numeric>
-#include<ext/rope>
-
-#define test() freopen("in","r",stdin)
-
+#define test() freopen("in", "r", stdin)
 using namespace std;
-using namespace __gnu_cc;
+unordered_set<char> st;
+#define ISOP(c) (st.count(c))
+typedef struct TreeNode *tptr;
+struct TreeNode
+{
+    tptr left = NULL, right = NULL;
+    char data;
+    TreeNode(char c) : data(c) {}
+};
+
+void post(tptr root){
+    if(!root)return;
+    if(root->left) post(root->left);
+    if(root->right) post(root->right);
+    cout << root->data << " ";
+}
+
 int main(int argc, char const *argv[])
 {
-    
+    /* code */
+    test();
+    string s;
+    st.insert('*'), st.insert('+');
+    cin >> s;
+    stack<tptr> stack_;
+    for (auto &i : s)
+    {
+        tptr tmp = new TreeNode(i);
+        if (!ISOP(i))
+        {
+            stack_.push(tmp);
+        }
+        else
+        {
+            tptr t1, t2, t3 = new TreeNode(i);
+            t1 = stack_.top(), stack_.pop(), t2 = stack_.top(), stack_.pop();
+            t3->left = t2, t3->right = t1;
+            stack_.push(t3);
+        }
+    }
+    post(stack_.top());
+
     return 0;
 }
