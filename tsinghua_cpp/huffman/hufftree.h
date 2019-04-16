a8fbafcb0dfe3531735d55bc829c9c0ce8e0a5a0
sonaspy <sonaspy@zju.edu.cn>
2019-04-17 02:17:33 +0800

    removal

--- tsinghua_cpp/huffman/hufftree.h
+++
@@ -1,8 +0,0 @@
-/******************************************************************************************
-
- ******************************************************************************************/
-
-#pragma once
-
-/*DSA*///typedef BinTree<HuffChar> HuffTree; //Huffman树，由BinTree派生，节点类型为HuffChar
-#define HuffTree BinTree<HuffChar> //Huffman树，由BinTree派生，节点类型为HuffChar
--- tsinghua_cpp/skiplist/quadlist_insert.h
+++
@@ -1,9 +0,0 @@
-/******************************************************************************************
-
- ******************************************************************************************/
-
-#pragma once
-
-template <typename T> QlistNodePosi(T) //将e作为p的后继、b的上邻插入Quadlist
-Quadlist<T>::insertAfterAbove ( T const& e, QlistNodePosi(T) p, QlistNodePosi(T) b = NULL )
-{  _size++; return p->insertAsSuccAbove ( e, b );  } //返回新节点位置（below = NULL）
