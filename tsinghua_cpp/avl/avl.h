ba84b6161c8640dad01e3ea3e40c9a108739c562
NewGuonx <gnx0406@gmail.com>
2019-04-17 01:51:46 +0800

    update

--- tsinghua_cpp/avl/avl.h
+++ tsinghua_cpp/avl/avl.h
@@ -5,11 +5,13 @@
 #pragma once
 
 #include "../BST/BST.h" //基于BST实现AVL树
-template <typename T> class AVL : public BST<T> { //由BST派生AVL树模板类
+template <typename T>
+class AVL : public BST<T>
+{ //由BST派生AVL树模板类
 public:
-   BinNodePosi(T) insert ( const T& e ); //插入（重写）
-   bool remove ( const T& e ); //删除（重写）
-// BST::search()等其余接口可直接沿用
+    BinNodePosi(T) insert(const T &e); //插入（重写）
+    bool remove(const T &e);           //删除（重写）
+    // BST::search()等其余接口可直接沿用
 };
 
 #include "AVL_implementation.h"
