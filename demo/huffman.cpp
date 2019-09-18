ec1492d55968c9c0a52bfa09c8cbf3a5686c4d65
sonaspy <new@sonaspy.org>
2019-09-18 11:07:03 +0800

    update

--- demo/huffman.cpp
+++ demo/huffman.cpp
@@ -1,16 +1,33 @@
-// author - sonaspy@outlook.com  
-// coding - utf_8 
+// author - sonaspy@outlook.com
+// coding - utf_8
 
-#include"../cpp/huffman.h"
-#include
-#define test() freopen("in.txt","r",stdin)
+#include "../cpp/huffman.h"
 using namespace dsa;
-
 int main(int argc, char const *argv[])
 {
     /* code */
     //test();
     FREQ_table mp;
-    
+    FILE *fp = fopen("./in.txt", "a+");
+    char c;
+    if (!fp)
+    {
+        cout << "Open File Failed !\n";
+        exit(0);
+    }
+    while (!feof(fp))
+    {
+        c = fgetc(fp);
+        cout << c << endl;
+        mp[c]++;
+    }
+    fclose(fp);
+
+    for (auto kv : mp)
+    {
+        cout << kv.first << " -> " << kv.second << endl;
+    }
+    ifstream fin("in.txt");
+
     return 0;
 }
