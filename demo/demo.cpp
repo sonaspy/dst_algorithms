7cabfc555ccf9c8e05180cb4c1d6f33e58d2adb7
sonaspy <new@sonaspy.org>
2019-09-23 15:46:47 +0800

    update

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,10 +1,10 @@
 // author -sonaspy@outlook.com
 // coding - utf_8
 
-#include "../src/dsa.h"
+#include <bits/stdc++.h>
 #define test() freopen("in", "r", stdin)
 using namespace std;
-using namespace dsa;
+
 #define SIZE 20
 int main(int argc, char const *argv[])
 {
@@ -17,11 +17,12 @@ int main(int argc, char const *argv[])
     // iota(a.begin(), a.end(), 0);
     clock_t startTime, endTime;
     startTime = clock();
-    string s = "0101011";
-    bitset<32> bs(s);
-    cout << bs.to_ulong() << endl;
-    cout << bs << endl;
-    cout << endl;
+    ifstream fin("innn.txt");
+    if (fin.fail())
+    {
+        cout << "failed\n";
+    }
+    fin.close();
     endTime = clock();
     cout << "The elapsed time is: " << (double)(endTime - startTime) / 1000 << "ms" << endl;
     return 0;
