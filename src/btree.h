913fd57a8863b3077338bccc22b9c15274539df0
sonaspy <new@sonaspy.org>
2019-09-24 21:18:49 +0800

    btree

--- src/btree.h
+++ src/btree.h
@@ -18,12 +18,12 @@ protected:
 #define K_MIN (_order - 1)
 #define C_MAX (2 * _order)
 #define C_MIN (_order)
-#define overflow(p) (((p)->keysize == K_MAX))
-#define underflow(p) (((p)->keysize == K_MIN))
+#define overflow(p) (((p)->sizeOfkey == K_MAX))
+#define underflow(p) (((p)->sizeOfkey == K_MIN))
     inline bnode_ptr<_Tp> __new_bnode(bool isleaf = true)
     {
         bnode_ptr<_Tp> v = new bnode<_Tp>(isleaf);
-        v->keysize = 0;
+        v->sizeOfkey = 0;
         v->key.resize(K_MAX);
         v->child.resize(C_MAX);
         fill(v->child.begin(), v->child.end(), nullptr);
@@ -39,9 +39,9 @@ protected:
     inline void _output_node(bnode_ptr<_Tp> v)
     {
         cout << " [ ";
-        for (int i = 0; i < v->keysize; i++)
+        for (int i = 0; i < v->sizeOfkey; i++)
             cout << v->key[i] << " ";
-        cout << "(" << v->keysize << ")";
+        cout << "(" << v->sizeOfkey << ")";
         cout << "] ";
     }
     void __inorder(bnode_ptr<_Tp> _root)
@@ -49,21 +49,21 @@ protected:
         if (!_root)
             return;
         int i;
-        for (i = 0; i < _root->keysize; i++)
+        for (i = 0; i < _root->sizeOfkey; i++)
         {
             __inorder(_root->child[i]);
             cout << _root->key[i] << " -> ";
         }
-        for (; i < _root->keysize + 1; i++)
+        for (; i < _root->sizeOfkey + 1; i++)
             __inorder(_root->child[i]);
     }
     void __nonfull_insert(bnode_ptr<_Tp> v, const _Tp &val)
     {
-        int idx = __upper_bound(v->key.begin(), v->key.begin() + v->keysize, val) - v->key.begin();
+        int idx = __upper_bound(v->key.begin(), v->key.begin() + v->sizeOfkey, val) - v->key.begin();
         if (v->isleaf)
         {
             v->key.insert(v->key.begin() + idx, val);
-            v->keysize++;
+            v->sizeOfkey++;
             v->key.pop_back();
         }
         else
@@ -80,43 +80,43 @@ protected:
     {
         bnode_ptr<_Tp> oldnode = p->child[c_idx];
         bnode_ptr<_Tp> node = __new_bnode(oldnode->isleaf);
-        node->keysize = K_MIN;
+        node->sizeOfkey = K_MIN;
         copy(oldnode->key.begin() + C_MIN, oldnode->key.begin() + C_MIN + K_MIN, node->key.begin());
         if (!oldnode->isleaf)
             copy(oldnode->child.begin() + C_MIN, oldnode->child.begin() + C_MIN * 2, node->child.begin());
         p->key.insert(p->key.begin() + c_idx, oldnode->key[K_MIN]);
-        p->keysize++;
+        p->sizeOfkey++;
         p->child.insert(p->child.begin() + c_idx + 1, node);
-        oldnode->keysize = K_MIN;
+        oldnode->sizeOfkey = K_MIN;
         p->child.pop_back();
         p->key.pop_back();
     }
     void __erase(bnode_ptr<_Tp> v, const _Tp &val)
     {
-        int _pos = __lower_bound(v->key.begin(), v->key.begin() + v->keysize, val) - v->key.begin();
+        int _pos = __lower_bound(v->key.begin(), v->key.begin() + v->sizeOfkey, val) - v->key.begin();
         if (v->isleaf)
         {
             if (v->key[_pos] == val)
             {
                 v->key.erase(v->key.begin() + _pos);
-                v->keysize--;
+                v->sizeOfkey--;
                 v->key.resize(K_MAX);
             }
         }
         else
         {
             bnode_ptr<_Tp> rc = nullptr, p = nullptr, lc = v->child[_pos];
-            if (_pos < v->keysize)
+            if (_pos < v->sizeOfkey)
                 rc = v->child[_pos + 1];
             if (v->key[_pos] == val)
             {
-                if (lc->keysize > K_MIN)
+                if (lc->sizeOfkey > K_MIN)
                 {
                     _Tp k1 = lc->precessor();
                     __erase(lc, k1);
                     v->key[_pos] = k1;
                 }
-                else if (rc->keysize > K_MIN)
+                else if (rc->sizeOfkey > K_MIN)
                 {
                     _Tp k1 = rc->successor();
                     __erase(rc, k1);
@@ -132,13 +132,13 @@ protected:
             {
                 if (_pos > 0)
                     p = v->child[_pos - 1];
-                if (lc->keysize == K_MIN)
+                if (lc->sizeOfkey == K_MIN)
                 {
-                    if (_pos > 0 && p->keysize > K_MIN)
+                    if (_pos > 0 && p->sizeOfkey > K_MIN)
                     {
                         __shift2rc(v, _pos - 1, p, lc);
                     }
-                    else if (_pos < v->keysize && rc->keysize > K_MIN)
+                    else if (_pos < v->sizeOfkey && rc->sizeOfkey > K_MIN)
                     {
                         __shift2lc(v, _pos, lc, rc);
                     }
@@ -159,13 +159,13 @@ protected:
     inline void __mergechild(bnode_ptr<_Tp> v, int i)
     {
         bnode_ptr<_Tp> lc = v->child[i], rc = v->child[i + 1];
-        lc->keysize = K_MAX;
+        lc->sizeOfkey = K_MAX;
         lc->key[K_MIN] = v->key[i];
         copy(rc->key.begin(), rc->key.begin() + K_MAX - 1 - K_MIN, lc->key.begin() + K_MIN + 1);
         if (!lc->isleaf)
             copy(rc->child.begin(), rc->child.begin() + C_MAX - C_MIN, lc->child.begin() + C_MIN);
         v->key.erase(v->key.begin() + i);
-        v->keysize--;
+        v->sizeOfkey--;
         v->child.erase(v->child.begin() + i + 1);
 
         v->key.resize(K_MAX);
@@ -175,32 +175,32 @@ protected:
     inline void __shift2rc(bnode_ptr<_Tp> x, int i, bnode_ptr<_Tp> y, bnode_ptr<_Tp> z)
     {
         z->key.insert(z->key.begin(), x->key[i]);
-        z->keysize++;
+        z->sizeOfkey++;
         z->key.pop_back();
 
-        x->key[i] = y->key[y->keysize - 1];
+        x->key[i] = y->key[y->sizeOfkey - 1];
 
         if (!z->isleaf)
         {
-            z->child.insert(z->child.begin(), y->child[y->keysize]);
+            z->child.insert(z->child.begin(), y->child[y->sizeOfkey]);
             z->child.pop_back();
         }
-        y->keysize--;
+        y->sizeOfkey--;
     }
     inline void __shift2lc(bnode_ptr<_Tp> x, int i, bnode_ptr<_Tp> y, bnode_ptr<_Tp> z)
     {
-        y->keysize++;
-        y->key[y->keysize - 1] = x->key[i];
+        y->sizeOfkey++;
+        y->key[y->sizeOfkey - 1] = x->key[i];
         x->key[i] = z->key[0];
 
-        z->keysize--;
+        z->sizeOfkey--;
         z->key.erase(z->key.begin());
 
         z->key.resize(K_MAX);
 
         if (!z->isleaf)
         {
-            y->child[y->keysize] = z->child[0];
+            y->child[y->sizeOfkey] = z->child[0];
             z->child.erase(z->child.begin());
 
             z->child.resize(C_MAX);
@@ -233,7 +233,7 @@ public:
         _last = nullptr;
         while (v)
         {
-            auto it = __lower_bound(v->key.begin(), v->key.begin() + v->keysize, val);
+            auto it = __lower_bound(v->key.begin(), v->key.begin() + v->sizeOfkey, val);
             if (it != v->key.begin() + v->key.size() && *it == val)
                 return v;
             _last = v, v = v->child[it - v->key.begin()];
@@ -263,7 +263,7 @@ public:
         bnode_ptr<_Tp> v = search(val);
         if (!v)
             return false;
-        if (_root->keysize == 1)
+        if (_root->sizeOfkey == 1)
         {
             if (_root->isleaf)
             {
