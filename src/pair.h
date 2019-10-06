401fa60c748ef437e2e0d2e0f46386e986feaa01
sonaspy <new@sonaspy.org>
2019-10-07 00:44:07 +0800

    update

---
+++ src/pair.h
@@ -0,0 +1,15 @@
+#ifndef ___DST_PAIR_H
+#define ___DST_PAIR_H
+
+#include "dst_config.h"
+#include "dst_relops.h"
+#include "dst_pair.h"
+
+#ifdef __DST_USE_NAMESPACES
+
+using __DSA::make_pair;
+using __DSA::pair;
+
+#endif /* __DST_USE_NAMESPACES */
+
+#endif /* ___DST_PAIR_H */
