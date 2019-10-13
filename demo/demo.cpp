2582cda85eca8ef40e9c2622ae3bad4c424c4c73
sonaspy <new@sonaspy.org>
2019-10-13 18:02:49 +0800

    update

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -7,19 +7,12 @@
 
 using namespace std;
 
-template <class iter>
-typename iter::value_type func(iter i)
-{
-    return *i;
-}
-
 int main(int argc, char const *argv[])
 {
     /* code */
     //test();
-    vector<vector<int>> l(10);
-    iterator_traits<int *>::value_type i = 10;
-    __is_random_access_iterator<int>;
+
+
 
     return 0;
 }
