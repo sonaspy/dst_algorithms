f13b20a7a96d5cd79a8c49fb63403a27ca283681
sonaspy <new@sonaspy.org>
2019-10-16 10:42:06 +0800

    update

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -5,12 +5,18 @@
 #include <bits/stdc++.h>
 #define test() freopen("in", "r", stdin)
 using namespace std;
-
-
+void fun()
+{
+}
 int main(int argc, char const *argv[])
 {
     /* code */
     //test();
 
     return 0;
+}
+
+void *p function()
+{
+    return (void *)fun;
 }
