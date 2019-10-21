d074ff493298aa79fbbd0a201f52820d3622f248
sonaspy <new@sonaspy.org>
2019-10-21 18:53:34 +0800

    update

--- demo/test.cpp
+++ demo/test.cpp
@@ -2,16 +2,15 @@
 // coding - utf_8
 
 #define SIZE 100
-#include "../src/deque.h"
+
 #include <stdio.h>
 #define test() freopen("in", "r", stdin)
-
-using namespace dsa;
 
 int main(int argc, char const *argv[])
 {
     /* code */
     //test();
-    
+    printf("hello world\n");
+
     return 0;
 }
