e022b5561c49f362bc585326ed38f74009d6ea6d
sonaspy <new@sonaspy.org>
2019-10-27 12:16:59 +0800

    update

--- b-+tree/btree/btree_test_flags.cc
+++ b-+tree/btree/btree_test_flags.cc
@@ -13,8 +13,6 @@
 // limitations under the License.
 
 #include "gflags/gflags.h"
-
-DEFINE_int32(test_values, 10000,
-             "The number of values to use for tests.");
+DEFINE_int32(test_values, 10000, "The number of values to use for tests.");
 DEFINE_int32(benchmark_values, 1000000,
              "The number of values to use for benchmarks.");
--- b-+tree/btree/safe_btree_test.cc
+++ b-+tree/btree/safe_btree_test.cc
@@ -33,83 +33,85 @@ namespace {
 
 template <typename K, int N>
 void SetTest() {
-  typedef TestAllocator<K> TestAlloc;
-  BtreeTest<safe_btree_set<K, std::less<K>, std::allocator<K>, N>, std::set<K> >();
-  BtreeAllocatorTest<safe_btree_set<K, std::less<K>, TestAlloc, N> >();
+    typedef TestAllocator<K> TestAlloc;
+    BtreeTest<safe_btree_set<K, std::less<K>, std::allocator<K>, N>,
+              std::set<K>>();
+    BtreeAllocatorTest<safe_btree_set<K, std::less<K>, TestAlloc, N>>();
 }
 
 template <typename K, int N>
 void MapTest() {
-  typedef TestAllocator<K> TestAlloc;
-  BtreeTest<safe_btree_map<K, K, std::less<K>, std::allocator<K>, N>, std::map<K, K> >();
-  BtreeAllocatorTest<safe_btree_map<K, K, std::less<K>, TestAlloc, N> >();
-  BtreeMapTest<safe_btree_map<K, K, std::less<K>, std::allocator<K>, N> >();
+    typedef TestAllocator<K> TestAlloc;
+    BtreeTest<safe_btree_map<K, K, std::less<K>, std::allocator<K>, N>,
+              std::map<K, K>>();
+    BtreeAllocatorTest<safe_btree_map<K, K, std::less<K>, TestAlloc, N>>();
+    BtreeMapTest<safe_btree_map<K, K, std::less<K>, std::allocator<K>, N>>();
 }
 
-TEST(SafeBtree, set_int32_32)   { SetTest<int32_t, 32>(); }
-TEST(SafeBtree, set_int32_64)   { SetTest<int32_t, 64>(); }
-TEST(SafeBtree, set_int32_128)  { SetTest<int32_t, 128>(); }
-TEST(SafeBtree, set_int32_256)  { SetTest<int32_t, 256>(); }
-TEST(SafeBtree, set_int64_256)  { SetTest<int64_t, 256>(); }
+TEST(SafeBtree, set_int32_32) { SetTest<int32_t, 32>(); }
+TEST(SafeBtree, set_int32_64) { SetTest<int32_t, 64>(); }
+TEST(SafeBtree, set_int32_128) { SetTest<int32_t, 128>(); }
+TEST(SafeBtree, set_int32_256) { SetTest<int32_t, 256>(); }
+TEST(SafeBtree, set_int64_256) { SetTest<int64_t, 256>(); }
 TEST(SafeBtree, set_string_256) { SetTest<std::string, 256>(); }
-TEST(SafeBtree, set_pair_256)   { SetTest<std::pair<int, int>, 256>(); }
-TEST(SafeBtree, map_int32_256)  { MapTest<int32_t, 256>(); }
-TEST(SafeBtree, map_int64_256)  { MapTest<int64_t, 256>(); }
+TEST(SafeBtree, set_pair_256) { SetTest<std::pair<int, int>, 256>(); }
+TEST(SafeBtree, map_int32_256) { MapTest<int32_t, 256>(); }
+TEST(SafeBtree, map_int64_256) { MapTest<int64_t, 256>(); }
 TEST(SafeBtree, map_string_256) { MapTest<std::string, 256>(); }
-TEST(SafeBtree, map_pair_256)   { MapTest<std::pair<int, int>, 256>(); }
+TEST(SafeBtree, map_pair_256) { MapTest<std::pair<int, int>, 256>(); }
 
 TEST(SafeBtree, Comparison) {
-  const int kSetSize = 1201;
-  safe_btree_set<int64_t> my_set;
-  for (int i = 0; i < kSetSize; ++i) {
-    my_set.insert(i);
-  }
-  safe_btree_set<int64_t> my_set_copy(my_set);
-  EXPECT_TRUE(my_set_copy == my_set);
-  EXPECT_TRUE(my_set == my_set_copy);
-  EXPECT_FALSE(my_set_copy != my_set);
-  EXPECT_FALSE(my_set != my_set_copy);
+    const int kSetSize = 1201;
+    safe_btree_set<int64_t> my_set;
+    for (int i = 0; i < kSetSize; ++i) {
+        my_set.insert(i);
+    }
+    safe_btree_set<int64_t> my_set_copy(my_set);
+    EXPECT_TRUE(my_set_copy == my_set);
+    EXPECT_TRUE(my_set == my_set_copy);
+    EXPECT_FALSE(my_set_copy != my_set);
+    EXPECT_FALSE(my_set != my_set_copy);
 
-  my_set.insert(kSetSize);
-  EXPECT_FALSE(my_set_copy == my_set);
-  EXPECT_FALSE(my_set == my_set_copy);
-  EXPECT_TRUE(my_set_copy != my_set);
-  EXPECT_TRUE(my_set != my_set_copy);
+    my_set.insert(kSetSize);
+    EXPECT_FALSE(my_set_copy == my_set);
+    EXPECT_FALSE(my_set == my_set_copy);
+    EXPECT_TRUE(my_set_copy != my_set);
+    EXPECT_TRUE(my_set != my_set_copy);
 
-  my_set.erase(kSetSize - 1);
-  EXPECT_FALSE(my_set_copy == my_set);
-  EXPECT_FALSE(my_set == my_set_copy);
-  EXPECT_TRUE(my_set_copy != my_set);
-  EXPECT_TRUE(my_set != my_set_copy);
+    my_set.erase(kSetSize - 1);
+    EXPECT_FALSE(my_set_copy == my_set);
+    EXPECT_FALSE(my_set == my_set_copy);
+    EXPECT_TRUE(my_set_copy != my_set);
+    EXPECT_TRUE(my_set != my_set_copy);
 
-  safe_btree_map<std::string, int64_t> my_map;
-  for (int i = 0; i < kSetSize; ++i) {
-    my_map[std::string(i, 'a')] = i;
-  }
-  safe_btree_map<std::string, int64_t> my_map_copy(my_map);
-  EXPECT_TRUE(my_map_copy == my_map);
-  EXPECT_TRUE(my_map == my_map_copy);
-  EXPECT_FALSE(my_map_copy != my_map);
-  EXPECT_FALSE(my_map != my_map_copy);
+    safe_btree_map<std::string, int64_t> my_map;
+    for (int i = 0; i < kSetSize; ++i) {
+        my_map[std::string(i, 'a')] = i;
+    }
+    safe_btree_map<std::string, int64_t> my_map_copy(my_map);
+    EXPECT_TRUE(my_map_copy == my_map);
+    EXPECT_TRUE(my_map == my_map_copy);
+    EXPECT_FALSE(my_map_copy != my_map);
+    EXPECT_FALSE(my_map != my_map_copy);
 
-  ++my_map_copy[std::string(7, 'a')];
-  EXPECT_FALSE(my_map_copy == my_map);
-  EXPECT_FALSE(my_map == my_map_copy);
-  EXPECT_TRUE(my_map_copy != my_map);
-  EXPECT_TRUE(my_map != my_map_copy);
+    ++my_map_copy[std::string(7, 'a')];
+    EXPECT_FALSE(my_map_copy == my_map);
+    EXPECT_FALSE(my_map == my_map_copy);
+    EXPECT_TRUE(my_map_copy != my_map);
+    EXPECT_TRUE(my_map != my_map_copy);
 
-  my_map_copy = my_map;
-  my_map["hello"] = kSetSize;
-  EXPECT_FALSE(my_map_copy == my_map);
-  EXPECT_FALSE(my_map == my_map_copy);
-  EXPECT_TRUE(my_map_copy != my_map);
-  EXPECT_TRUE(my_map != my_map_copy);
+    my_map_copy = my_map;
+    my_map["hello"] = kSetSize;
+    EXPECT_FALSE(my_map_copy == my_map);
+    EXPECT_FALSE(my_map == my_map_copy);
+    EXPECT_TRUE(my_map_copy != my_map);
+    EXPECT_TRUE(my_map != my_map_copy);
 
-  my_map.erase(std::string(kSetSize - 1, 'a'));
-  EXPECT_FALSE(my_map_copy == my_map);
-  EXPECT_FALSE(my_map == my_map_copy);
-  EXPECT_TRUE(my_map_copy != my_map);
-  EXPECT_TRUE(my_map != my_map_copy);
+    my_map.erase(std::string(kSetSize - 1, 'a'));
+    EXPECT_FALSE(my_map_copy == my_map);
+    EXPECT_FALSE(my_map == my_map_copy);
+    EXPECT_TRUE(my_map_copy != my_map);
+    EXPECT_TRUE(my_map != my_map_copy);
 }
 
 } // namespace
