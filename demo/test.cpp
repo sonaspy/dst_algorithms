e7610a93bd6176b1290b0b682cdc2eb16cf10515
sonaspy <new@sonaspy.org>
2020-04-10 17:53:58 +0800

    update

--- demo/test.cpp
+++ demo/test.cpp
@@ -1,11 +1,15 @@
 // author - sonaspy@outlook.com
 // coding - utf_8
+#include<bits/stdc++.h>
 
+using namespace std;
 #define SIZE 100
-#include <stdio.h>
+
+
+
 int main(int argc, char const *argv[]) {
     /* code */
     // test();
-
+    cout<< 1;
     return 0;
 }
